901f9c473ca6a886eecb4a2a9b61690afa72d376
Shane McCulley <smcculle@uno.edu>
2015-09-26 15:16:59 -0500

    fixed test case errors, todo: verify index integrity

--- csv2plain.py
+++ csv2plain.py
@@ -1,5 +1,8 @@
-import ast
+import json
 import traceback
+
+#todo:  ensure consistent index by testing against paragraph elements,
+#some elements appearing as characters might be changing the offset.  
 
 def insert(old, new, i):
     #in log, index starts at 1
@@ -12,38 +15,56 @@ def delete(old, si, ei):
     return old[:si] + old[ei:]
 
 def write(s):
-    with open('pt.txt', 'w') as f:
+    of = filename.replace('output/', '')
+    of = of.replace('_out', '')
+    with open('o_' + of, 'w') as f:
         f.write(s)
+        
+def get_dict(line):
+    try:
+        i = line.index('{')
+        log_dict = json.loads(line[i:])
+        return log_dict
+    except:
+        raise
 
-with open('output/454_456_out.txt', 'r') as f:
+filename = 'output/1_456_out.txt'    
+with open(filename, 'r') as f:
     log = f.read().split('\n')
+    
 #s.decode('string_escape')
 #p = re.compile(r'\\u00\w\w')  for intermixed unicode marks
 
 plain_text = ''
-logdict = ast.literal_eval(log[log.index('chunkedSnapshot') + 1])
-cs_string = logdict['string']
-cs_string = cs_string.decode('unicode-escape')    
-plain_text += cs_string
+logdict = get_dict(log[log.index('chunkedSnapshot') + 1])
+
+#should not contain a string if log starts at revision 1
+if 'string' in logdict:
+    chunk_string = logdict['string']
+    chunk_string = chunk_string.decode('unicode-escape')    
+    plain_text += chunk_string
+
+#skip default style init for now for plain text
 cl_index = log.index('changelog') + 1
 
 for line in log[cl_index:]:    
     try:
-        si = line.index('{')
-        actiondict = ast.literal_eval(line[si:])
+        
+        actiondict = get_dict(line)
     
         if actiondict['type'] == 'is':
             i = actiondict['insert_before_index']
             s = actiondict['string']
-            print i,s
+            #print 'at',ind,i,s
             plain_text = insert(plain_text, s, i)
             
         elif actiondict['type'] == 'ds':
-            print 'not happening'
             si = actiondict['starting_index']
             ei = actiondict['ending_index']
+            #print 'd:', si, ei
             plain_text = delete(plain_text, si, ei)
     except:
         pass #get rid of last empty line
-    
-write(plain_text)
+
+#some paragraph style elements outside 128 ascii range
+write(plain_text.encode('mbcs'))
