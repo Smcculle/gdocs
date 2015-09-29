da0dac2bd66da699366161da1af526f7a1782f8c
Shane McCulley <smcculle@uno.edu>
2015-09-29 15:03:19 -0500

    slides working

--- drivestats.py
+++ drivestats.py
@@ -4,9 +4,10 @@ from collections import Counter
 from datetime import timedelta
 import json
 
-#todo*:  Index starting better for slide logs not beginning at 1
+#todo*:  Index starting better for slide logs not beginning at 1, control output with flag (console vs log)
 #todo:  map gid to account name through retrieving history with changelog.
 #todo:  stats for pages that don't start at revision 1
+#todo: refactor similar code into 1 function
 #var historyUrl = baseUrl + docId + "/revisions/history?id=" + docId + "&start=1&end=-1&zoom_level=0&token=" + token
 
 def get_list(line):
@@ -15,46 +16,26 @@ def get_list(line):
     stop_index -= 1  #stops an empty element at the end of the list
     return line[:stop_index].split('|')
 
-def temp(t):
-    counter = Counter()
-    session_counter = Counter()
-   # data = get_data(filename)
-    first_line = get_list(data[0])
-    start_time = int(first_line[0])/1000
-    counter[first_line[1]] += 1
-    counter[first_line[3]] += 1
-    counter[first_line[5]] += 1
-    revision_dict = {}
-    prev_revision = -1
-    for line in data[1:-1]:
-        
-        line = get_list(line)
-        google_id = line[1]
-        revision = line[2]
-        sid = line[3]
-        action = line[5]
-
-        if prev_revision != revision:
-            counter['revisions'] += 1
-            counter[google_id] += 1
-            session_counter[sid] += 1
-        if sid not in revision_dict and sid != 'None':
-            revision_dict[sid] = google_id
-            
-        counter[action] +=1
-        prev_revision = revision
-
 def print_stats(counter, session_counter, action_counter, revision_dict, start_time, end_time):
+    '''Prints a list of stats to the console'''
     counter['revisions'] += 1    
     fmt = "%Y-%m-%d %H:%M:%S"
     authors = list(set(revision_dict.values()))
-    timediff = timedelta(seconds=(end_time - start_time))
-    revperday = counter['revisions'] / (timediff.total_seconds() / 86400)
-    longest_session, longest_session_revs = session_counter.most_common(1)[0]  
+    timediff = timedelta(seconds=(time.mktime(time.localtime()) - start_time))
+    if timediff.days > 0:
+        revperday = counter['revisions'] / (timediff.total_seconds() / 86400)
+        days = timediff.days
+    else:
+        revperday = counter['revisions']
+        days = 0
+    (hours, seconds) = divmod(timediff.seconds, 3600)
+    longest_session, longest_session_revs = session_counter.most_common(1)[0]
 
-    print 'File created on %s' % (time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time)))
+    #modify this to write to file. 
+    #sys.stdout = open(filename, 'w')
+    print 'File created on %s' % (time.strftime(fmt, time.localtime(start_time)))
     print 'Last revision on %s' % (time.strftime(fmt, time.localtime(end_time)))
-    print 'Age of file: %s' % timediff
+    print 'Age of file: %d days, %d hours, %d minutes' % (days, hours, seconds/60)
     print 'File contains %d total revisions, broken down by google id:' % (counter['revisions'])
     for author in authors:
         print '\t %s with %s revisions' % (author, counter[author])
@@ -66,12 +47,43 @@ def print_stats(counter, session_counter, action_counter, revision_dict, start_t
         print '\t', action, ':', action_counter[action]
 
 def parse_slide(data):
-    data = data[5:]
+    action_dict = {15: 'add_text', 4:'multiset_op', 16:'del_text', 3:'add_box',
+             12:'add_slide', 13:'del_slide', 0:'del_box', 35:'revert'}
     data = json.loads(data)
     data = data['changelog']
+    counter = Counter()
+    session_counter = Counter()
+    action_counter = Counter()
+    
+    start_time = data[0][1]/1000
+    end_time = data[-1][1]/1000
 
-    data = data[1:]
-   
+    revision_dict = {}
+    prev_revision = -1
+    counter['revisions'] = -1
+    
+    if not data[0][5]:
+        data = data[1:]
+
+    for line in data:
+        try:
+            action = action_dict[line[0][0]]
+        except KeyError:
+            action = 'unknown'
+        time = line[1]
+        google_id = line[2]
+        revision = line[3]
+        sid = line[4]
+
+        counter['revisions'] += 1
+        counter[google_id] += 1
+        session_counter[sid] += 1
+        if sid not in revision_dict and sid != 'None':
+            revision_dict[sid] = google_id
+            
+        action_counter[action] += 1
+    print_stats(counter, session_counter, action_counter, revision_dict, start_time, end_time)
+    
 def parse_doc(data):
     
     data = data.split('\n')
