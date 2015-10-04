22b259fc79f40b7c58fcdad113fe9b5f8d08cdbe
Shane McCulley <smcculle@uno.edu>
2015-10-04 00:47:11 -0500

    various helper tests

--- kumodocs/getlog.py
+++ kumodocs/getlog.py
@@ -1,5 +1,5 @@
 import gdata.docs.service
-import ConfigParser, urllib2, os, httplib2, sys
+import ConfigParser, urllib2, os, httplib2, sys, urllib
 
 from apiclient.discovery import build
 from oauth2client.file import Storage
@@ -65,9 +65,10 @@ def create_URL( fileID, start, end ):
   print url
   return url                                          
 
-def list_files():
-  mime_type = 'application/vnd.google-apps.folder'
-  #service.files().list()
+def list_files(key):
+  search_param = {'doc':"mimeType = 'application/vnd.google-apps.document'", 'slide': "mimeType = 'application/vnd.google-apps.presentation'"}
+  
+  files = service.files().list(q=search_param[key]).execute()
   '''
   result = []
   page_token = None
@@ -129,8 +130,24 @@ if user_info != None:
   username = user_info['user']['emailAddress']
 else:
   print "Can't get username"
-	
-''' using service._http to place a post request:
+
+'''firefox_headers = Host: docs.google.com
+User-Agent: Mozilla/5.0 (Windows NT 6.0; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+Accept-Language: en-US,en;q=0.5
+Accept-Encoding: gzip, deflate
+X-Same-Domain: 1
+X-Build: kix_2015.39-Tue_c
+X-Rel-Id: f4.3c139c9a.s
+Content-Type: application/x-www-form-urlencoded;charset=utf-8
+Referer: https://docs.google.com/document/d/1SsCaJuY51VVeCmvh80obb7kPsb6Ybau6ngKm8KIUxps/edit
+Content-Length: 185
+Cookie: S=documents=ppqauTAY-3fd0JQbccmuLg; PREF=ID=1111111111111111:FF=0:LD=en:TM=1430849915:LM=1434946035:GM=1:S=DvnVckHxB-q8f3uM; NID=72=SN-h7bU5ixF7JXVToenbW0A8Hx29fx9ok685PuJ3tL4NQJG3IH_0Vxs4pb5yYhzbRJQVIWfkMeH86qxNinQxRO640EVjrl7Gxx5T16oZE6WygOUo1PNvFo-CerLb-eZAiA4TAawyioMYbu-gnBdIydUV2ReMXNPo7c787dtdBADCLOt9Q90b2umTdE_xm3B42w0167Eh8Is07c42jZAnS61KZ6_kN6ICbc2CruSEqd_207xGHEKbJZivYZQ_QHuUKn1bQRzCDeXKshJEJOkxYrOYflTBuDsm_4oZWHE; SID=DQAAAAUBAAB0RJLyqQ8N3ctjEOV27goo-GxsLlWD_eZT80GRWp7HGVVhWBUQRKfrKUZIiBoZ6K-cIETf6RZI-bMSA5F35KZxJnXMSJyR9hvqlnicb_4_3-0XeU7TdMrtGn-OIzUuKIc1-qfIBXPGnJl1R8jvx0dJYnJPNkh4i4hpwT8c3t_aMeMeKZ5HoovdeOnzr3iKZYOcX1-6aTDvDITPF-lMDnrwZguB5zitFvBfwE6kEPnwrxnIILqJiku44fxKucwUsg8ikdP0jdq7V_H95G0lVrU7gSKQi-GMPx5Ye1UEZkMak_2lwv5lz6UT2rjuZOFT9tGOrwmgiu5HG2RUYQiey4UAIwwDVLoqQfYdEc1Jcv0k6A; HSID=AYmQls4T18mDaCbr2; SSID=AmXPoDc67aYDoNmzl; APISID=iKZE9vv7HT70tHOf/ADLlc-pDqQppsjzXn; SAPISID=f1uyG_q5zERmOJZG/At2H1U7DnWgRErkCH; OGP=-5061451:; WRITELY_SID=DQAAAAsBAACvdY0F8RM5A6LtQizf9xNQ66vw6JhGiSWJ6TXfFTIgH3QIUQzW5XV6VhqBSbidSQ-WUdJIdXPkxCtfiLc8ybwhgByfQs5jxQiQhuqfpfX-tJ35wCFEGzkv84Bsw1KYL0QR5Wo-sr0c8rQUOnOVYFCs8F5cWUsGqQrOOgUQ8rUcqBV-hypdymQyEks5mL6bqaOu3-ZH19sNCd4vkQMsUbADa5ZfDtyERBe-JEOtLOYq-w6V3_Hys9L7qqZE1Ld0sKsqECzb0GOrCJ5scmaT_NLCy8UC7a3mQFb-Tmp23STPU5QFMc1FRkRWSj0MPVyCw2jGWeX84ppYdEgZQoAYlyFYhFt3J1oDK3MOv__WBguhaw; GMAIL_RTT=79; llbcs=0; lbcs=1
+Connection: keep-alive
+Pragma: no-cache
+Cache-Control: no-cache'''
+
+'''using service._http to place a post request:
 headers = {'Content-Length': ' *len(data)*', 'Accept-Language': ' en-US,en;q=0.5',
            'Accept-Encoding': ' gzip, deflate', 'X-Rel-Id': ' f3.3c139c9a.s', 'Connection': ' keep-alive',
            'Accept': ' text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -149,15 +166,76 @@ data is then urlencoded such as urllib.encode(data)
 resp, cont = service._http.request(
     url, method='POST', body=data, headers=my_headers)
     '''
+content_type = 'application/x-www-form-urlencoded;charset=UTF-8'
+Origin = 'https://docs.google.com'
+Referer = 'https://docs.google.com/document/d/1SsCaJuY51VVeCmvh80obb7kPsb6Ybau6ngKm8KIUxps/edit'
+user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
+x_build = 'kix_2015.39-Tue_c'
+x_rel_id = 'f4.1222e5f4.s'
+x_same_domain = 1
+#i_cid_0 = '1dvj9ynBuUWMi9NyMdlS3GgD37_HZVGKxBy5cRQ'
+i_cid_0 = '1-1lmZiUX7N0wdSobr8v0-Wzo2PHezCXQyjEEeg'
+fid = '1bR8kOZ7MB1-RO3ENrPau7k1JrdjbALq38D6TlgXWqAo'
+data = {}
+data['r0'] = ['image',{'cosmoId':i_cid_0, 'container':fid}]
+req_body = {}
+req_body['renderOps'] = data
+req_body = urllib.urlencode(req_body)
+req_body.replace('+', '')
+render_url = base_url + fid + '/renderdata?id=' + fid
+my_headers = {}
+#my_headers['Content-Length'] = len(req_body)
+my_headers['Content-Type'] = content_type
+#my_headers['Referer'] = Referer
+
 try:
+  #resp, cont = service._http.request(render_url, method='POST', body=req_body, headers=my_headers)
+  myComments = service._http.request('https://www.googleapis.com/drive/v2/files/1SsCaJuY51VVeCmvh80obb7kPsb6Ybau6ngKm8KIUxps/comments?includeDeleted=true')
+  
   #url = 'https://docs.google.com/document/d/1SsCaJuY51VVeCmvh80obb7kPsb6Ybau6ngKm8KIUxps/renderdata?id=1SsCaJuY51VVeCmvh80obb7kPsb6Ybau6ngKm8KIUxps&token=AC4w5Vh_iTzvW2ih7wThCGejbO3J1pVpkw%3A1443374604500'
   #url = 'https://lh3.googleusercontent.com/TG-HofNaScWRUfyUuA-Ty4PzbZUW8UXHktI9apIv-DQygu7CJkjkgGY5Cm3X7FSXCiAzlQ=s1600'
   #url = 'https://lh3.googleusercontent.com/8oDqSwcKXBvy6tFS4badAWeQk7_1o3a_6d6EY9EKFsC9pTWD55qXXK3JCi8jcjTuisk01zv6ejeztTqXX3NpE91C5XxLFrT_CLUCK65bskNceclBXM4ersjGSuO0u_3dHFQ=s1600'
   #url = 'filesystem:https://docs.google.com/persistent/docs/documents/1SsCaJuY51VVeCmvh80obb7kPsb6Ybau6ngKm8KIUxps/image/AE7MojedZEHNulGfoIidNJe3igHFde64_lGK2V7B6HrsqCmiBz1olI2xkcLwSNqaMsa76Xa69Z375zIli6pI1QSQKSpOsklAx4VirdmOLf0hxkCCa1HRdu8zW0JqsGD4xR-GOAq9NXLW3pvU6TcFnzO26iAphdEZLQS9c5CiznsfeRJZsCid56IS95UE_RDnip1zwZMXgTqOrP1RXUSY9frirqSPCC6YJUDiJ0h2LcaRmnolSe2r_ndW0f8ml0UOkiBTc99UHNFPYkHJ14mbIgPLVGjvOWd2QQ?zx=t2cfm4hfa0gb' 
-  url = create_URL(fid, 292, 293)
+  #url = create_URL(fid, 292, 293)
   #url = 'https://docs.google.com/document/d/1SsCaJuY51VVeCmvh80obb7kPsb6Ybau6ngKm8KIUxps/'
-  resp, content = service._http.request(url)
+  #resp, content = service._http.request(url)
   #print resp
 	    
 except: 
   raise
+
+#To catch a comment#
+
+comments = service.comments().list(fileId=fid, includeDeleted=True).execute()
+for i in comments['items']:
+	print i['htmlContent']
+	for j in i['replies']:
+		print j['content']
+		
+
+def main(argv):
+
+    helpmsg = 'Usage: python drivestats.py <inputfile>. Takes single output log from log2csv or raw changelog from slides\n'\
+              'Ex: \tpython drivestats.py output/1_254_out.txt  or\n'\
+              '  \tpython drivestats.py slogs/1_317.txt'
+    if not argv:
+        print helpmsg
+        sys.exit(2)
+    else:
+        filename = argv[0]
+
+    if not os.path.isfile(filename):
+        print 'No file found.', helpmsg
+        sys.exit(2)
+
+    with open(filename, 'r') as infile:
+        data = infile.read()
+
+    #indicates slide log
+    if data[0] == ')':
+        parse_slide(data[5:], filename)
+    else:
+        parse_doc(data, filename)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
