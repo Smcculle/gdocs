b5b186f3830977a1eaa9bacabb986cf32bec7840
Shane McCulley <smcculle@uno.edu>
2016-03-29 14:34:45 -0500

    removed duplicate

--- mappings.py
+++ mappings.py
@@ -7,12 +7,9 @@ _MAPPINGS = {
     "si": "start_index",
     "ei": "end_index",
     "is": "ins",
-    "iss": "sug_ins",
-    "dss": "sug_del",
-    "astss": "sug_adj_style",
     "ds": "del",
     "as": "adj_style",
-    "fm": "fm", #
+    "fm": "fm",  #
     "timestamp": "timestamp",
     "UID": "UID",
     "rev": "rev",
@@ -21,11 +18,11 @@ _MAPPINGS = {
     "sm": "style_mod",
     "mts": "multiset",
     "sugid": "sug_id",
-    "tbs_al": "table_aln", #
-    "tbs_of": "table_off", #
+    "tbs_al": "table_aln",  #
+    "tbs_of": "table_off",  #
     "das_a": "datasheet_anchor",
-    "ps_ltr": "ps_ltr", #
-    "hs_nt": "heading_style_normal_text", 
+    "ps_ltr": "ps_ltr",  #
+    "hs_nt": "heading_style_normal_text",
     "ds_pw": "page_width",
     "ds_ph": "page_height",
     "ds_mb": "marg_bottom",
@@ -56,8 +53,8 @@ _MAPPINGS = {
     "ts_fs_i": "font_size_i",
     "ts_it": "italic",
     "ts_it_i": "italic_i",
-    "ts_sc": "small_caps", 
-    "ts_sc_i": "small_caps_i", 
+    "ts_sc": "small_caps",
+    "ts_sc_i": "small_caps_i",
     "ts_st": "strikethrough",
     "ts_st_i": "strikethrough_i",
     "ts_un": "underline",
@@ -66,34 +63,34 @@ _MAPPINGS = {
     "ts_va_i": "vert_align_i",
     "ps_al": "alignment",
     "ps_al_i": "alignment_i",
-    "ps_awao": "ps_awao", #
-    "ps_awao_i": "ps_awao_i", #
+    "ps_awao": "ps_awao",  #
+    "ps_awao_i": "ps_awao_i",  #
     "ps_hd": "heading_style",
     "ps_hdid": "heading_id",
     "ps_ifl": "indent_first_line",
     "ps_ifl_i": "indent_first_line_i",
     "ps_il": "indent_line",
     "ps_il_i": "indent_line_i",
-    "ps_ir": "ps_ir", #
-    "ps_ir_i": "ps_ir_i", #
-    "ps_klt": "ps_klt", #
-    "ps_klt_i": "ps_klt_i", #
-    "ps_kwn": "ps_kwn", #
-    "ps_kwn_i": "ps_kwn_i", #
+    "ps_ir": "ps_ir",  #
+    "ps_ir_i": "ps_ir_i",  #
+    "ps_klt": "ps_klt",  #
+    "ps_klt_i": "ps_klt_i",  #
+    "ps_kwn": "ps_kwn",  #
+    "ps_kwn_i": "ps_kwn_i",  #
     "ps_ls": "line_space",
     "ps_ls_i": "line_space_i",
     "ps_sa": "space_af_pgraph",
     "ps_sa_i": "space_af_pgraph_i",
     "ps_sb": "space_bf_pgraph",
     "ps_sb_i": "space_bf_pgraph_i",
-    "ps_sm": "ps_sm", #
-    "ps_sm_i": "ps_sm_i", #
-    "ps_ts": "ps_ts", #
-    "cv": "cv", #
+    "ps_sm": "ps_style",
+    "ps_sm_i": "ps_style_i",  #
+    "ps_ts": "ps_ts",  #
+    "cv": "cv",  #
     "opValue": "opValue",
     "op": "op",
     "opIndex": "opIndex",
-    "ds_hi": "ds_hi", #
+    "ds_hi": "ds_hi",  #
     "sugid": "sug_id",
     "id": "id",
     "null": "null",
@@ -106,24 +103,26 @@ _MAPPINGS = {
     "ue": "update_elem",
     "te": "tether_elem",
     "mlti": "mlti",
-    
-    #mostly related to insertion of kix objects in page
-    "ls_nest": "ls_nest", #
-    "spi": "spi", #
-    "epm": "epm", #
+    "i_ht": "img_ht",
+    "i_wth": "img_wth",
+    "i_src": "img_src",
+    "i_cid": "img_cosmoId",
+
+    # mostly related to insertion of kix objects in page
+    "ls_nest": "ls_nest",  #
+    "spi": "spi",  #
+    "epm": "epm",  #
     "et": "et",  #
-    "ls_ts": "ls_ts", #
-    "ls_id": "ls_id", #
-    "ee_eo": "ee_eo", #
-    "le_nb": "le_nb", #
-    "hfe_hft": "hfe_hft", #
-    }
+    "ls_ts": "ls_ts",  #
+    "ls_id": "ls_id",  #
+    "ee_eo": "ee_eo",  #
+    "le_nb": "le_nb",  #
+    "hfe_hft": "hfe_hft",  #
+}
 
 ''' unknown properties
---involves bullet lists
 
---
-eo_type  element something type 
+eo_type  element something type
 b_a
 eo_mb  element something margin bottom
 eo_ad  ?
@@ -131,37 +130,19 @@ eo_mt  margin top
 eo_mr  margin right
 eo_ml  margin left
 -- involves lists
-le_nb contains 
-    nl_3
-    nl_2
-    nl_1
-    nl_0
-    nl_7
-    nl_6
-    nl_5
-    nl_4
-    nl_8
-each one of nl_1...8 contains properties describing formats of indention level
+le_nb contains
+    nl_0 - nl_8, each one of nl_1...8 contains properties describing formats of indention level
 b_gf  %0...%8 for nl_0 ... nl_8
 b_ifl (indent first line)
 b_gs  associated with bullet list
-b_gt  
+b_gt
 b_ts associated with numbered lists
 b_il  amount to indent each level
 
 list insertion associated with epm: le_nb
 --- '''
+
+
 def remap(old_key):
-    return _MAPPINGS[old_key]
-
-def load_keys():
-    with open('keys.txt', 'r') as f:
-        key_data = f.read()
-        for key in key_data.split('\n'):
-            _MAPPINGS[key] = key
-
-load_keys()
-                
-
-
-
+    """ Remaps given key if present in mapping dictionary, or returns old_key otherwise """
+    return _MAPPINGS.get(old_key, old_key)
