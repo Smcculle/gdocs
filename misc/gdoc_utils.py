3cf7997cd8f63a15204b059d170c6eb993d78263
Shane McCulley <smcculle@uno.edu>
2017-02-13 21:43:35 -0600

    Collection of common IO/GUI/data utilities

---
+++ misc/gdoc_utils.py
@@ -0,0 +1,63 @@
+import Tkinter as Tk
+import errno
+import ntpath
+import os
+import re
+import shutil
+import tempfile
+import tkFileDialog
+from contextlib import contextmanager
+
+
+def strip_invalid_characters(filename):
+    """ Very conservative stripping of extraneous characters in filename """
+    return re.sub('[^\w\-_. ]', '_', filename)
+
+
+def strip_path_extension(path):
+    """ Returns filename by stripping extension from basename"""
+    basename = ntpath.basename(path)
+    return os.path.splitext(basename)[0]
+
+
+def choose_file_dialog(**options):
+    """ Creates an open file dialog to choose a file, and returns a handle to that file """
+    root = Tk.Tk()
+    root.geometry('200x200+200+200')
+    root.attributes('-alpha', 0.0)
+    root.lift()
+    root.focus_force()
+    chosen_file = tkFileDialog.askopenfile(**options)
+    root.destroy()
+    return chosen_file
+
+
+def ensure_path(path):
+    """ Attempts to make a directory and raises exception if there is an issue"""
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        #  # if the directory exists, ignore error
+        if exception.errno != errno.EEXIST:
+            raise
+
+
+def remove_directory(path):
+    """ Deletes all files and removes directory at path"""
+    try:
+        shutil.rmtree(path)
+    except IOError as e:
+        # if the directory does not exist, ignore error
+        if e.errno != errno.ENOENT:
+            print 'I/O error removing temp files at {}'.format(path)
+            raise
+
+
+@contextmanager
+def temp_directory():
+    """ Creates and removes temporary directory using WITH statement """
+    path = tempfile.mkdtemp()
+    try:
+        yield path
+    finally:
+        remove_directory(path)
