77ed64cdffb877d93a6267c98e0068431e46f38c
Shane McCulley <smcculle@uno.edu>
2017-04-17 16:23:44 -0500

    Moved package-level namedtuples FileChoice, Drawing. Added DRAW_PARAMS Modified API_BASE format keys to be consistent with FileChoice

--- kumodocs/gsuite/__init__.py
+++ kumodocs/gsuite/__init__.py
@@ -1,7 +1,14 @@
+from collections import namedtuple
+
 # package-level constants defined below
 REL_CONFIG_PATH = ['..', '..', 'config', 'config.cfg']
-API_BASE = 'https://docs.google.com/{service}/d/{id}/{params}'
+API_BASE = 'https://docs.google.com/{drive}/d/{file_id}/{params}'
 REV_PARAMS = 'revisions/load?start={start}&end={end}'
 RENDER_PARAMS = 'renderdata?id={id}'
 MIME_TYPE = "mimeType = 'application/vnd.google-apps.{}'"
 SERVICES = ['document', 'drawing', 'form', 'presentation', 'spreadsheet']
+DRAW_PARAMS = 'image?w={w}&h={h}'
+
+# package-level named tuples
+FileChoice = namedtuple('FileChoice', 'file_id, title, drive, max_revs')
+Drawing = namedtuple('Drawing', 'd_id width height')
