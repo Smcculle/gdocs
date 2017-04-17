30acc0e171342d716f6d33787a96939b9db3e5a4
Shane McCulley <smcculle@uno.edu>
2017-04-16 21:05:21 -0500

    Init files for python package structure
    Package-level constants defined in gsuite/__init__.py

---
+++ kumodocs/gsuite/__init__.py
@@ -0,0 +1,7 @@
+# package-level constants defined below
+REL_CONFIG_PATH = ['..', '..', 'config', 'config.cfg']
+API_BASE = 'https://docs.google.com/{service}/d/{id}/{params}'
+REV_PARAMS = 'revisions/load?start={start}&end={end}'
+RENDER_PARAMS = 'renderdata?id={id}'
+MIME_TYPE = "mimeType = 'application/vnd.google-apps.{}'"
+SERVICES = ['document', 'drawing', 'form', 'presentation', 'spreadsheet']
